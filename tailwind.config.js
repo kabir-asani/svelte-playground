bc677cd34a55e2051d7d43053915616427b5897a
Kabir Asani <iamkabirasani@gmail.com>
2024-01-29 01:48:05 +0530

    Add screen breaks + custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,28 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {},
+		screens: {
+			sm: '576px', // Mobile
+			md: '768px', // Tablet
+			lg: '1024px', // Laptop
+			xl: '1280px' // Desktop
+		}
 	},
 	plugins: [require('daisyui')],
 	daisyui: {
-		themes: ['dark']
+		themes: [
+			{
+				chaos: {
+					primary: '#141414',
+					'primary-content': '#fff',
+					secondary: '#171717',
+					accent: '#453F9A',
+					neutral: '#9C9C9C',
+					'base-100': '#000',
+					'base-content': '#fff'
+				}
+			}
+		]
 	}
 };
