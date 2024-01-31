8f80ea747b9fdf808b8548fd7fac1a52220bc729
Kabir Asani <iamkabirasani@gmail.com>
2024-01-31 17:47:13 +0530

    Update SectionColumnBody.svelte

--- src/lib/components/shared/section/section-body/SectionColumnBody.svelte
+++ src/lib/components/shared/section/section-body/SectionColumnBody.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-	export let properties: String = '';
-</script>
-
-<div class={'flex flex-col w-full gap-4' + ' ' + properties.valueOf()}>
+<div class={'flex flex-col w-full gap-4' + ' ' + $$restProps.class || ''}>
 	<slot />
 </div>
